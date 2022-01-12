0f3b89190ecd59dd5bca4816fd425f0048fae60e
pl0m <daria-edel@rambler.ru>
2022-01-12 22:11:38 +0100

    tried out first content scraping

--- scraper-chromium.py
+++ scraper-chromium.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
 import undetected_chromedriver as uc
 
 import time
+import os
+import shutil
+import re
 
 def main():
     #chop = webdriver.ChromeOptions
@@ -11,10 +15,25 @@ def main():
     driver = uc.Chrome()
     
     
-    #
-    driver.get('https://video.ethz.ch/')
     
-    time.sleep(1000)
+    #driver.get('https://video.ethz.ch/lectures/d-arch/2021/autumn/052-0505-00L.html')
+    driver.get('https://video.ethz.ch/lectures.html')
+    
+    #  below code  get urls of mp4 elements https://video.ethz.ch/lectures.series-metadata.json 
+    
+    #from selenium.webdriver.common.by import By
+    
+    time.sleep(5)
+
+    download_date = driver.find_elements(By.CSS_SELECTOR, 'time')[1].text
+    #download_link = driver.find_element(By.CSS_SELECTOR, '.video a').get_attribute("href")
+    
+    print(download_date)
+    #print(download_link)
+
+
+#path = r
+    time.sleep(10)
 
 
 
