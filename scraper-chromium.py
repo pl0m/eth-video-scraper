e1c254fd3de6143677f667b2e0b31642a2d36174
pl0m <daria-edel@rambler.ru>
2022-01-17 19:53:26 +0100

    first working scaper

--- scraper-chromium.py
+++ scraper-chromium.py
@@ -10,37 +10,49 @@ import shutil
 import re
 
 def main():
-    #chop = webdriver.ChromeOptions
-    
     uc.TARGET_VERSION = 97
     driver = uc.Chrome()
     
+    # Get all departments with videos on the ETH video platform
+    #scrape_departments(driver)
     
+    # Get all years of a specific department
+    scrape_years(driver, 'd-arch')
     
-    #driver.get('https://video.ethz.ch/lectures/d-arch/2021/autumn/052-0505-00L.html')
-    driver.get('https://video.ethz.ch/lectures.html')
-    
-    #  below code  get urls of mp4 elements https://video.ethz.ch/lectures.series-metadata.json 
-    
-    #from selenium.webdriver.common.by import By
-    
-    time.sleep(5)
-
-    download_date = driver.find_elements(By.CSS_SELECTOR, 'time')[1].text
-    #download_link = driver.find_element(By.CSS_SELECTOR, '.video a').get_attribute("href")
-    
-    print(download_date)
-    #print(download_link)
-
-
-#path = r
     time.sleep(10)
 
 
+def scrape_departments(driver):
+    driver.get('https://video.ethz.ch/lectures.html')
+    
+    # Get all link elements
+    department_links = driver.find_elements(By.CSS_SELECTOR, '.newsListBox a')
+    
+    for department_link in department_links:
+        department_href = department_link.get_attribute('href')
+        print(department_href)
 
+
+
+def scrape_years(driver, department):
+    driver.get(f'https://video.ethz.ch/{department.lower()}.html')
+    
+    # Get all link elements
+    years_links = driver.find_elements(By.CSS_SELECTOR, '.newsListBox a')
+    
+    for year_link in years_links:
+        year_href = year_link.get_attribute('href')
+        print(year_href)
 
 
 
 
 if __name__ == "__main__":
     main()
+
+#download_date = driver.find_elements(By.CSS_SELECTOR, 'time')[1].text
+#download_link = driver.find_element(By.CSS_SELECTOR, '.video a').get_attribute("href")
+#driver.get('https://video.ethz.ch/lectures/d-arch/2021/autumn/052-0505-00L.html')
+#options = webdriver.FirefoxOptions()
+#options.log.level = "trace"
+#options.headless = True
